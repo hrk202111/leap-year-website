160945f132d9e47f203c1b8e9a96a55b8d71b90b
hrk202111 <hrk202111@gmail.com>
2021-01-22 11:52:31 +0600

    fuck

---
+++ script.js
@@ -0,0 +1,9 @@
+function checkLeapYear(){
+    var leapYear = document.getElementById(`year`).value;
+    if((leapYear % 4 == 0) && (leapYear % 100 != 0) || (leapYear % 400 == 0)){
+        alert (leapYear +`is a leap year`);    
+    }
+    else{
+        alert(leapYear +`is not a leap year`)
+    }
+}
